d6f7076b9c1606b8833772332e2f90398d1fc79b
Flexible Games <simtropbuggi@gmail.com>
2024-02-27 16:34:20 -0600

    Starting Recipe System

---
+++ code/VintageEngineering/RecipeSystem/VERecipeLoader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace VintageEngineering.RecipeSystem
+{    
+    public class VERecipeLoader : ModSystem
+    {
+        ICoreServerAPI sapi;
+        private bool classExclusiveRecipes = true;
+
+        public override double ExecuteOrder()
+        {
+            return 1;
+        }
+
+        public override bool ShouldLoad(EnumAppSide forSide)
+        {
+            return forSide == EnumAppSide.Server;
+        }
+
+        public override void AssetsLoaded(ICoreAPI api)
+        {
+            sapi = api as ICoreServerAPI;
+            if (sapi == null) return;
+            classExclusiveRecipes = sapi.World.Config.GetBool("classExclusiveRecipes", true);
+
+            // Now for all the recipe loading...
+        }
+    }
+}
