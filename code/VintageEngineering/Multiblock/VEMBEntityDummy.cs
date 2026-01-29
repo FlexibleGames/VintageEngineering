b0ff72431f6da279d351abad8ab0cf791609259e
Flexible Games <simtropbuggi@gmail.com>
2026-01-28 18:59:57 -0600

    Update MB Dummy block with new type and GetInterface<T> override.

--- code/VintageEngineering/Multiblock/VEMBEntityDummy.cs
+++ code/VintageEngineering/Multiblock/VEMBEntityDummy.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 
 namespace VintageEngineering.Multiblock
@@ -13,12 +14,32 @@ namespace VintageEngineering.Multiblock
     /// </summary>
     public class VEMBEntityDummy : BlockEntity
     {
-        private Vec3i _offset;
+        private Vec3i _offset = new Vec3i();
 
         /// <summary>
         /// Offset of this block from the Core of the Multiblock
         /// </summary>
         public Vec3i Offset { get { return _offset; } }
+
+        public void SetOffset(Vec3i offset) => _offset = offset;
+        public void SetOffset(Vec4i offset)
+        {
+            _offset.X = offset.X;
+            _offset.Y = offset.Y;
+            _offset.Z = offset.Z;
+        }
+
+        public override void ToTreeAttributes(ITreeAttribute tree)
+        {
+            base.ToTreeAttributes(tree);
+            tree.SetVec3i("offset", _offset);
+        }
+
+        public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldAccessForResolve)
+        {
+            base.FromTreeAttributes(tree, worldAccessForResolve);
+            _offset = tree.GetVec3i("offset");
+        }
 
     }
 }
