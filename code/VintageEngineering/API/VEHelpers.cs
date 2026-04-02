0398be1a446b39fe87d495fc4fcdb555f3b3e5b6
Flexible Games <simtropbuggi@gmail.com>
2026-04-01 19:17:29 -0500

    Fix #71  Bucket logic with Mixer

--- code/VintageEngineering/API/VEHelpers.cs
+++ code/VintageEngineering/API/VEHelpers.cs
@@ -1,6 +1,8 @@
 ﻿using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Config;
 using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
 
 namespace VintageEngineering.API
 {
@@ -47,6 +49,88 @@ namespace VintageEngineering.API
             else return false;
 
             return true;
+        }
+
+        public static int SplitStackAndPerformAction(ICoreAPI l_api, Entity byEntity, ItemSlot slot, System.Func<ItemStack, int> action)
+        {
+            if (slot.Itemstack == null)
+            {
+                return 0;
+            }
+            if (slot.Itemstack.StackSize == 1)
+            {
+                int num = action(slot.Itemstack);
+                if (num > 0)
+                {
+                    int maxStackSize = slot.Itemstack.Collectible.MaxStackSize;
+                    EntityPlayer entityPlayer = byEntity as EntityPlayer;
+                    if (entityPlayer == null)
+                    {
+                        return num;
+                    }
+                    entityPlayer.WalkInventory(delegate (ItemSlot pslot)
+                    {
+                        if (pslot.Empty || pslot is ItemSlotCreative || pslot.StackSize == pslot.Itemstack.Collectible.MaxStackSize)
+                        {
+                            return true;
+                        }
+                        int mergableq = slot.Itemstack.Collectible.GetMergableQuantity(slot.Itemstack, pslot.Itemstack, EnumMergePriority.DirectMerge);
+                        if (mergableq == 0)
+                        {
+                            return true;
+                        }
+                        BlockLiquidContainerBase blockLiquidContainerBase = slot.Itemstack.Collectible as BlockLiquidContainerBase;
+                        BlockLiquidContainerBase invLiqBlock = pslot.Itemstack.Collectible as BlockLiquidContainerBase;
+                        int? num3;
+                        if (blockLiquidContainerBase == null)
+                        {
+                            num3 = null;
+                        }
+                        else
+                        {
+                            ItemStack content = blockLiquidContainerBase.GetContent(slot.Itemstack);
+                            num3 = ((content != null) ? new int?(content.StackSize) : null);
+                        }
+                        int? num4 = num3;
+                        int valueOrDefault = num4.GetValueOrDefault();
+                        int? num5;
+                        if (invLiqBlock == null)
+                        {
+                            num5 = null;
+                        }
+                        else
+                        {
+                            ItemStack content2 = invLiqBlock.GetContent(pslot.Itemstack);
+                            num5 = ((content2 != null) ? new int?(content2.StackSize) : null);
+                        }
+                        num4 = num5;
+                        if (valueOrDefault != num4.GetValueOrDefault())
+                        {
+                            return true;
+                        }
+                        slot.Itemstack.StackSize += mergableq;
+                        pslot.TakeOut(mergableq);
+                        slot.MarkDirty();
+                        pslot.MarkDirty();
+                        return true;
+                    });
+                }
+                return num;
+            }
+            ItemStack containerStack = slot.Itemstack.Clone();
+            containerStack.StackSize = 1;
+            int num2 = action(containerStack);
+            if (num2 > 0)
+            {
+                slot.TakeOut(1);
+                EntityPlayer entityPlayer2 = byEntity as EntityPlayer;
+                if (entityPlayer2 == null || !entityPlayer2.Player.InventoryManager.TryGiveItemstack(containerStack, true))
+                {
+                    l_api.World.SpawnItemEntity(containerStack, byEntity.SidedPos.XYZ, null);
+                }
+                slot.MarkDirty();
+            }
+            return num2;
         }
     }
 }
--- code/VintageEngineering/Blocks/BlockFluidIO.cs
+++ code/VintageEngineering/Blocks/BlockFluidIO.cs
@@ -7,10 +7,12 @@ using VintageEngineering.API;
 using VintageEngineering.Electrical;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
 using Vintagestory.GameContent;
+using static Vintagestory.GameContent.BlockLiquidContainerBase;
 
 
 namespace VintageEngineering
@@ -27,64 +29,116 @@ namespace VintageEngineering
             {
                 return false;
             }
+            ItemSlot hotbarSlot = byPlayer?.InventoryManager?.ActiveHotbarSlot;
             BlockEntity bentity = null;
             if (blockSel.Position != null)
             {
                 bentity = world.BlockAccessor.GetBlockEntity(blockSel.Position);
             }
-            if (bentity == null || bentity is not IVELiquidInterface) return false;
+            if (bentity == null) return false;
             
-            if (byPlayer != null && byPlayer.InventoryManager.ActiveHotbarSlot != null && !byPlayer.InventoryManager.ActiveHotbarSlot.Empty)
+            if (byPlayer != null && hotbarSlot != null && !hotbarSlot.Empty)
             {
-                ILiquidSink bucket = byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack.Collectible as ILiquidSink;
-                if (bucket != null)
+                if (hotbarSlot.Itemstack.Collectible.Code.Path.Contains("stick")) // Stick to empty
                 {
-                    ItemStack contents = bucket.GetContent(byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack);
-                    if (contents != null)
+                    if (bentity is IVELiquidInterface ivela)
                     {
-                        DummySlot topush = new DummySlot(contents);
-                        IVELiquidInterface ivel = bentity as IVELiquidInterface;
-                        ItemSlotLiquidOnly push = ivel.GetLiquidAutoPushIntoSlot(blockSel.Face, topush);
-                        if (push == null) return true;
-                        WaterTightContainableProps props = BlockLiquidContainerBase.GetContainableProps(contents);
-                        int capacityavailable = (int)(push.CapacityLitres * props.ItemsPerLitre) - (int)(push.StackSize);
-                        if (capacityavailable >= contents.StackSize)
+                        BlockEntityContainer bec = bentity as BlockEntityContainer;
+                        if (bec != null)
                         {
-                            topush.TryPutInto(api.World, push, topush.StackSize);
-                            bucket.SetContent(byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack, null);                            
+                            foreach (int slotid in ivela.InputLiquidContainerSlotIDs)
+                            {
+                                if (bec.Inventory[slotid] != null && !bec.Inventory[slotid].Empty)
+                                { 
+                                    bec.Inventory[slotid]?.TakeOutWhole(); 
+                                }
+                            }
+                            foreach (int slotid in ivela.OutputLiquidContainerSlotIDs)
+                            {
+                                if (bec.Inventory[slotid] != null && !bec.Inventory[slotid].Empty)
+                                {
+                                    bec.Inventory[slotid]?.TakeOutWhole(); 
+                                }
+                            }
                         }
-                        else
-                        {
-                            int moved = topush.TryPutInto(api.World, push, topush.StackSize);
-                            contents.StackSize -= moved;
-                            bucket.SetContent(byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack, contents);
-                        }
-                        bentity.MarkDirty(true);
-                        return true;
-                    }
-                    else
-                    {
-                        IVELiquidInterface ivel = bentity as IVELiquidInterface;
-                        ItemSlotLiquidOnly pull = ivel.GetLiquidAutoPullFromSlot(blockSel.Face);
-                        if (pull == null) return true;
-                        WaterTightContainableProps props = BlockLiquidContainerBase.GetContainableProps(pull.Itemstack);
-                        int cancontain = (int)(bucket.CapacityLitres * props.ItemsPerLitre);
-                        if (cancontain >= pull.Itemstack.StackSize)
-                        {
-                            bucket.SetContent(byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack, pull.Itemstack.Clone());
-                            pull.TakeOutWhole();
-                        }
-                        else
-                        {
-                            ItemStack pulled = pull.Itemstack.Clone();
-                            pulled.StackSize = cancontain;
-                            bucket.SetContent(byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack, pulled.Clone());
-                            pull.TakeOut(cancontain);
-                        }
-                        bentity.MarkDirty(true);
-                        return true;
                     }
                 }
+                ILiquidSource bucket = hotbarSlot.Itemstack.Collectible as ILiquidSource;
+                if (bucket != null)
+                {
+                    if (!bucket.AllowHeldLiquidTransfer) return false;
+
+                    ItemStack contents = bucket.GetContent(hotbarSlot.Itemstack);
+                    if (contents != null && contents.StackSize > 0)
+                    {
+                        DummySlot topush;
+                        if (hotbarSlot.Itemstack.StackSize > 1)
+                        {
+                            ItemStack singlebucket = hotbarSlot.Itemstack.Clone();
+                            singlebucket.StackSize = 1;
+                            topush = new((singlebucket.Collectible as ILiquidSource).GetContent(singlebucket));
+                        }
+                        else
+                        {
+                            topush = new(contents);
+                        }
+                        IVELiquidInterface ivelb = bentity as IVELiquidInterface;
+                        ItemSlotLiquidOnly pushto = ivelb.GetLiquidAutoPushIntoSlot(blockSel.Face, topush);
+                        if (pushto == null) return true;
+                        WaterTightContainableProps propsa = BlockLiquidContainerBase.GetContainableProps(contents);
+                        int capacityavailable = (int)(pushto.CapacityLitres * propsa.ItemsPerLitre) - (int)(pushto.StackSize);
+
+                        ItemStack nummoved = topush.TakeOut(capacityavailable); //.TakeOut(topush.StackSize);
+                        
+                        if (nummoved != null && nummoved.StackSize > 0)
+                        {
+                            // add to pushto slot
+                            DummySlot ds = new DummySlot(nummoved);                            
+
+                            int paction = VEHelpers.SplitStackAndPerformAction(api, byPlayer.Entity, hotbarSlot, delegate (ItemStack stack)
+                            {
+                                bucket.TryTakeContent(stack, nummoved.StackSize);
+                                return nummoved.StackSize;
+                            });
+                            ds.TryPutInto(world, pushto, ds.Itemstack.StackSize);
+                            bentity.MarkDirty(true);
+                            return true;
+                        }
+                    }
+                }
+
+                IVELiquidInterface ivel = bentity as IVELiquidInterface;
+                ItemSlotLiquidOnly pull = ivel?.GetLiquidAutoPullFromSlot(blockSel.Face);
+                if (pull == null)
+                {
+                    BlockEntityContainer bec = bentity as BlockEntityContainer;
+                    foreach (int slot_id in ivel.InputLiquidContainerSlotIDs)
+                    {
+                        if (!bec.Inventory[slot_id].Empty) pull = bec.Inventory[slot_id] as ItemSlotLiquidOnly;
+                    }
+                }
+                
+                ILiquidSink sink = hotbarSlot.Itemstack.Collectible as ILiquidSink;
+
+                if (sink != null && pull != null)
+                {
+                    if (!sink.AllowHeldLiquidTransfer) return false;
+
+                    ItemStack owncontentstack = pull.Itemstack; // GetContent...
+                    if (owncontentstack == null) return base.OnBlockInteractStart(world, byPlayer, blockSel);
+
+                    ItemStack liquidstackforparticles = owncontentstack.Clone();
+
+                    float liters = GameMath.Max(sink.TransferSizeLitres, sink.CapacityLitres);
+                    int moved2 = VEHelpers.SplitStackAndPerformAction(api, byPlayer.Entity, hotbarSlot, (ItemStack stack) => sink.TryPutLiquid(stack, owncontentstack, liters));
+                    if (moved2 > 0)
+                    {
+                        pull.TakeOut(moved2);
+                        //DoLiquidMovedEffects(byPlayer, liquidstackforparticles, moved2, EnumLiquidDirection.Fill);
+                        bentity.MarkDirty(true);
+                        return true;
+                    }
+                }       
             }
 
             bool handled = base.OnBlockInteractStart(world, byPlayer, blockSel);
